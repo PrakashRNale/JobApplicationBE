11079107ffef34bf7cc0c4b10546165853ac9c09
PrakashRN <prakashcsedyp@gmail.com>
2025-01-10 17:06:41 +0530

    Added README.md and changed flow after deployment

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const { auth } = require("googleapis/build/src/apis/abusiveexperiencereport/inde
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  'http://localhost:8000/auth/google/callback'
+  process.env.CALLBACK_URL   //'http://localhost:8000/auth/google/callback'  use this for local
 );
 
 // Step 1: Generate Auth URL (Authorization Code Flow)
